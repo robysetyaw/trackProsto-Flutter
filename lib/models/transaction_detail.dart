2bdc1aa3bf9422c0cf16b8fc17283ec2fc14c15c
robysetyaw <robysetyaw@gmail.com>
2023-08-09 11:30:28 +0700

    add feat TransactionDetail model

---
+++ lib/models/transaction_detail.dart
@@ -0,0 +1,11 @@
+class TransactionDetail {
+  final String meatName;
+  final double price;
+  final int qty;
+
+  TransactionDetail({
+    required this.meatName,
+    required this.price,
+    required this.qty,
+  });
+}
