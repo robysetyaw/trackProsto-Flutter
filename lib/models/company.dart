6bfa57b4fefcdb40a21854b1d99c9ec7e088d16a
robysetyaw <robysetyaw@gmail.com>
2023-08-09 10:40:21 +0700

    add feat addCompany floating button and showDialog

--- lib/models/company.dart
+++ lib/models/company.dart
@@ -1,29 +1,23 @@
 class Company {
-  final String id;
   final String companyName;
   final String address;
   final String email;
   final String phoneNumber;
-  final bool isActive;
   // Anda bisa menambahkan atribut lain seperti created_at, updated_at, dsb jika diperlukan.
 
   Company({
-    required this.id,
     required this.companyName,
     required this.address,
     required this.email,
     required this.phoneNumber,
-    required this.isActive,
   });
 
   factory Company.fromJson(Map<String, dynamic> json) {
     return Company(
-      id: json['id'],
       companyName: json['company_name'],
       address: json['address'],
       email: json['email'],
       phoneNumber: json['phone_number'],
-      isActive: json['is_active'],
     );
   }
 }
--- lib/views/company_view.dart
+++ lib/views/company_view.dart
@@ -9,12 +9,90 @@ class CompanyView extends StatefulWidget {
 
 class _CompanyViewState extends State<CompanyView> {
   final _companyService = CompanyService();
+  final _formKey = GlobalKey<FormState>();
+  final _companyNameController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
+
   late Future<List<Company>> _companyFuture;
 
   @override
   void initState() {
     super.initState();
     _companyFuture = _companyService.getAllCompanies();
+  }
+
+  void _showAddCompanyDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add Company'),
+            content: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _companyNameController,
+                    decoration: InputDecoration(labelText: 'Company Name'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter company name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(labelText: 'Address'),
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    controller: _phoneNumberController,
+                    decoration: InputDecoration(labelText: 'Phone Number'),
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+              TextButton(
+                  child: Text('Add'),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final newCompany = Company(
+                        companyName: _companyNameController.text,
+                        address: _addressController.text,
+                        email: _emailController.text,
+                        phoneNumber: _phoneNumberController.text,
+                      );
+
+                      final success =
+                          await _companyService.addCompany(newCompany);
+
+                      if (success) {
+                        Navigator.of(context).pop();
+                        // Refresh data setelah menambahkan company baru
+                        setState(() {
+                          _companyFuture = _companyService.getAllCompanies();
+                        });
+                      } else {
+                        // Tampilkan pesan kesalahan
+                      }
+                    }
+                  }),
+            ],
+          );
+        });
   }
 
   @override
@@ -43,6 +121,11 @@ class _CompanyViewState extends State<CompanyView> {
             );
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddCompanyDialog,
+        child: Icon(Icons.add),
+        tooltip: 'Add Company',
       ),
     );
   }
