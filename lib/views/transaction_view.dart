e0e620f59339196342c355ae18c88f89335d52dd
robysetyaw <robysetyaw@gmail.com>
2023-08-09 11:29:34 +0700

    add feat addTransaction view

--- lib/views/transaction_view.dart
+++ lib/views/transaction_view.dart
@@ -17,6 +17,69 @@ class _TransactionViewState extends State<TransactionView> {
     _transactionFuture = _transactionService.getAllTransactions();
   }
 
+  void _showAddTransactionDialog() {
+    final _formKey = GlobalKey<FormState>();
+    String name = '';
+    String txType = 'in';
+    double paymentAmount = 0.0;
+    // ... (Anda bisa menambahkan variabel lain untuk detail transaksi jika diperlukan)
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add New Transaction'),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  onChanged: (value) => name = value,
+                  decoration: InputDecoration(labelText: 'Name'),
+                  validator: (value) => value!.isEmpty ? "Name is required" : null,
+                ),
+                TextFormField(
+                  onChanged: (value) => txType = value,
+                  decoration: InputDecoration(labelText: 'Transaction Type'),
+                ),
+                TextFormField(
+                  onChanged: (value) => paymentAmount = double.tryParse(value!) ?? 0.0,
+                  decoration: InputDecoration(labelText: 'Payment Amount'),
+                  keyboardType: TextInputType.number,
+                ),
+                // Tambahkan widget lain untuk input transaksi
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  final transactionData = {
+                    'name': name,
+                    'tx_type': txType,
+                    'payment_amount': paymentAmount,
+                    // 'transaction_details': transactionDetails,
+                  };
+                  _transactionService.addTransaction(transactionData);
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +100,17 @@ class _TransactionViewState extends State<TransactionView> {
                 final transaction = snapshot.data![index];
                 return ListTile(
                   title: Text(transaction.invoiceNumber),
-                  subtitle: Text(transaction.name + ", " + transaction.company),
+                  subtitle: Text(transaction.name),
                   trailing: Text(transaction.total.toString()),
                 );
               },
             );
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddTransactionDialog,
+        child: Icon(Icons.add),
       ),
     );
   }
